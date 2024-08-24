837a8491fd05720de604addc098682d0255a637c
Daniel Helm <dan.helm@gmail.com>
2024-08-24 10:53:46 -0500

    rename to fund accounts and cleanup

--- src/commands/helper/fund-accounts.ts
+++ src/commands/helper/fund-accounts.ts
@@ -1,8 +1,8 @@
+import {confirm, select} from '@inquirer/prompts'
 import {Command, Flags} from '@oclif/core'
+import chalk from 'chalk'
 import {ethers} from 'ethers'
 import path from 'node:path'
-import chalk from 'chalk'
-import {select, confirm} from '@inquirer/prompts'
 import {toString as qrCodeToString} from 'qrcode'
 
 import {parseTomlConfig} from '../../utils/config-parser.js'
@@ -15,7 +15,7 @@ enum Layer {
 
 const FUNDING_AMOUNT = 0.008
 
-export default class HelperFundContracts extends Command {
+export default class HelperFundAccounts extends Command {
   static description = 'Fund L1 and L2 accounts for contracts'
 
   static flags = {
@@ -28,6 +28,11 @@ export default class HelperFundContracts extends Command {
       default: './config.toml',
       description: 'Path to config.toml file',
     }),
+    dev: Flags.boolean({
+      char: 'd',
+      default: false,
+      description: 'Use Anvil devnet funding logic',
+    }),
     l1rpc: Flags.string({
       char: 'o',
       description: 'L1 RPC URL',
@@ -36,19 +41,14 @@ export default class HelperFundContracts extends Command {
       char: 't',
       description: 'L2 RPC URL',
     }),
-    dev: Flags.boolean({
-      char: 'd',
-      description: 'Use Anvil devnet funding logic',
-      default: false,
+    manual: Flags.boolean({
+      char: 'm',
+      description: 'Manually fund the accounts',
     }),
     pod: Flags.boolean({
       char: 'p',
       default: false,
       description: 'Run inside Kubernetes pod',
-    }),
-    manual: Flags.boolean({
-      char: 'm',
-      description: 'Manually fund the accounts',
     }),
     'private-key': Flags.string({
       char: 'k',
@@ -56,19 +56,20 @@ export default class HelperFundContracts extends Command {
     }),
   }
 
-  private l1Provider!: ethers.JsonRpcProvider
-  private l2Provider!: ethers.JsonRpcProvider
-  private l1Rpc!: string
-  private l2Rpc!: string
-  private fundingWallet!: ethers.Wallet
-  private l1ETHGateway!: string
   private blockExplorers: Record<Layer, {blockExplorerURI: string}> = {
     [Layer.L1]: {blockExplorerURI: ''},
     [Layer.L2]: {blockExplorerURI: ''},
   }
 
+  private fundingWallet!: ethers.Wallet
+  private l1ETHGateway!: string
+  private l1Provider!: ethers.JsonRpcProvider
+  private l1Rpc!: string
+  private l2Provider!: ethers.JsonRpcProvider
+  private l2Rpc!: string
+
   public async run(): Promise<void> {
-    const {flags} = await this.parse(HelperFundContracts)
+    const {flags} = await this.parse(HelperFundAccounts)
 
     const configPath = path.resolve(flags.config)
     const config = parseTomlConfig(configPath)
@@ -125,112 +126,6 @@ export default class HelperFundContracts extends Command {
     this.log(chalk.green('Funding complete'))
   }
 
-  private async fundL1Addresses(addresses: string[], flags: any): Promise<void> {
-    this.log(chalk.cyan('\nFunding L1 Addresses:'))
-    for (const address of addresses) {
-      if (!address) {
-        this.warn(`Address not found in config for one of the L1 accounts`)
-        continue
-      }
-
-      if (flags.dev) {
-        await this.fundAddressAnvil(this.l1Provider, address, FUNDING_AMOUNT, Layer.L1)
-      } else if (flags.manual) {
-        await this.promptManualFunding(address, FUNDING_AMOUNT, Layer.L1)
-      } else {
-        await this.fundAddressNetwork(this.l1Provider, address, FUNDING_AMOUNT, Layer.L1)
-      }
-    }
-  }
-
-  private async fundL2Addresses(addresses: string[], flags: any): Promise<void> {
-    this.log(chalk.cyan('\nFunding L2 Addresses:'))
-    for (const address of addresses) {
-      if (!address) {
-        this.warn(`Address not found in config for one of the L2 accounts`)
-        continue
-      }
-
-      const fundingMethod = await this.promptUserForL2Funding()
-
-      if (fundingMethod === 'bridge') {
-        await this.bridgeFundsL1ToL2(address, FUNDING_AMOUNT)
-      } else if (fundingMethod === 'direct') {
-        await this.fundAddressNetwork(this.l2Provider, address, FUNDING_AMOUNT, Layer.L2)
-      } else {
-        await this.promptManualFunding(address, FUNDING_AMOUNT, Layer.L2)
-      }
-    }
-  }
-
-  private async fundAddressAnvil(provider: ethers.JsonRpcProvider, address: string, amount: number, layer: Layer) {
-    try {
-      const result = await provider.send('anvil_setBalance', [address, ethers.parseEther(amount.toString()).toString()])
-      await this.logAddress(address, `Successfully funded with ${amount} ETH`, layer)
-      return result
-    } catch (error) {
-      this.error(
-        `Failed to fund ${address} (${layer} devnet): ${error instanceof Error ? error.message : 'Unknown error'}`,
-      )
-    }
-  }
-
-  private async fundAddressNetwork(provider: ethers.JsonRpcProvider, address: string, amount: number, layer: Layer) {
-    try {
-      const tx = await this.fundingWallet.sendTransaction({
-        to: address,
-        value: ethers.parseEther(amount.toString()),
-      })
-      await tx.wait()
-      await this.logTx(tx.hash, `Funded ${address} with ${amount} ETH`, layer)
-    } catch (error) {
-      this.error(`Failed to fund ${address} (${layer}): ${error instanceof Error ? error.message : 'Unknown error'}`)
-    }
-  }
-
-  private async promptManualFunding(address: string, amount: number, layer: Layer) {
-    const chainId =
-      layer === Layer.L1 ? (await this.l1Provider.getNetwork()).chainId : (await this.l2Provider.getNetwork()).chainId
-
-    let qrString = `ethereum:${address}@${chainId}&value=${amount}`
-
-    await this.logAddress(address, `Please fund the following address with ${chalk.yellow(amount)} ETH`, layer)
-    this.log('\n')
-    this.log(`ChainID: ${chalk.cyan(Number(chainId))}`)
-    this.log(`Chain RPC: ${chalk.cyan(layer === Layer.L1 ? this.l1Rpc : this.l2Rpc)}`)
-    this.log('\n')
-    this.log('Scan this QR code to fund the address:')
-
-    this.log(await qrCodeToString(qrString, {small: true, type: 'terminal'}))
-
-    let funded = false
-    while (!funded) {
-      await confirm({message: 'Press Enter when ready...'})
-      this.log(`Checking...`)
-      const balance = await (layer === Layer.L1 ? this.l1Provider : this.l2Provider).getBalance(address)
-      const formattedBalance = ethers.formatEther(balance)
-
-      if (Number(formattedBalance) >= amount) {
-        this.log(chalk.green(`Wallet Balance: ${formattedBalance}`))
-        funded = true
-      } else {
-        this.log(chalk.yellow(`Balance is only ${formattedBalance}. Please fund the wallet.`))
-      }
-    }
-  }
-
-  private async promptUserForL2Funding(): Promise<string> {
-    const answer = await select({
-      message: 'How would you like to fund the L2 address?',
-      choices: [
-        {name: 'Bridge funds from L1', value: 'bridge'},
-        {name: 'Directly fund L2 wallet', value: 'direct'},
-        {name: 'Manual funding', value: 'manual'},
-      ],
-    })
-    return answer
-  }
-
   private async bridgeFundsL1ToL2(recipient: string, amount: number): Promise<void> {
     try {
       this.log(chalk.cyan(`Bridging funds from L1 to L2 for recipient: ${recipient}`))
@@ -264,6 +159,78 @@ export default class HelperFundContracts extends Command {
     }
   }
 
+  private async fundAddressAnvil(provider: ethers.JsonRpcProvider, address: string, amount: number, layer: Layer) {
+    try {
+      const result = await provider.send('anvil_setBalance', [address, ethers.parseEther(amount.toString()).toString()])
+      await this.logAddress(address, `Successfully funded with ${amount} ETH`, layer)
+      return result
+    } catch (error) {
+      this.error(
+        `Failed to fund ${address} (${layer} devnet): ${error instanceof Error ? error.message : 'Unknown error'}`,
+      )
+    }
+  }
+
+  private async fundAddressNetwork(provider: ethers.JsonRpcProvider, address: string, amount: number, layer: Layer) {
+    try {
+      const tx = await this.fundingWallet.sendTransaction({
+        to: address,
+        value: ethers.parseEther(amount.toString()),
+      })
+      await tx.wait()
+      await this.logTx(tx.hash, `Funded ${address} with ${amount} ETH`, layer)
+    } catch (error) {
+      this.error(`Failed to fund ${address} (${layer}): ${error instanceof Error ? error.message : 'Unknown error'}`)
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private async fundL1Addresses(addresses: string[], flags: any): Promise<void> {
+    this.log(chalk.cyan('\nFunding L1 Addresses:'))
+    for (const address of addresses) {
+      if (!address) {
+        this.warn(`Address not found in config for one of the L1 accounts`)
+        continue
+      }
+
+      if (flags.dev) {
+        // eslint-disable-next-line no-await-in-loop
+        await this.fundAddressAnvil(this.l1Provider, address, FUNDING_AMOUNT, Layer.L1)
+      } else if (flags.manual) {
+        // eslint-disable-next-line no-await-in-loop
+        await this.promptManualFunding(address, FUNDING_AMOUNT, Layer.L1)
+      } else {
+        // eslint-disable-next-line no-await-in-loop
+        await this.fundAddressNetwork(this.l1Provider, address, FUNDING_AMOUNT, Layer.L1)
+      }
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private async fundL2Addresses(addresses: string[]): Promise<void> {
+    this.log(chalk.cyan('\nFunding L2 Addresses:'))
+    for (const address of addresses) {
+      if (!address) {
+        this.warn(`Address not found in config for one of the L2 accounts`)
+        continue
+      }
+
+      // eslint-disable-next-line no-await-in-loop
+      const fundingMethod = await this.promptUserForL2Funding()
+
+      if (fundingMethod === 'bridge') {
+        // eslint-disable-next-line no-await-in-loop
+        await this.bridgeFundsL1ToL2(address, FUNDING_AMOUNT)
+      } else if (fundingMethod === 'direct') {
+        // eslint-disable-next-line no-await-in-loop
+        await this.fundAddressNetwork(this.l2Provider, address, FUNDING_AMOUNT, Layer.L2)
+      } else {
+        // eslint-disable-next-line no-await-in-loop
+        await this.promptManualFunding(address, FUNDING_AMOUNT, Layer.L2)
+      }
+    }
+  }
+
   private async logAddress(address: string, description: string, layer: Layer): Promise<void> {
     const link = await addressLink(address, this.blockExplorers[layer])
     this.log(`${description}: ${chalk.cyan(link)}`)
@@ -272,5 +239,50 @@ export default class HelperFundContracts extends Command {
   private async logTx(txHash: string, description: string, layer: Layer): Promise<void> {
     const link = await txLink(txHash, this.blockExplorers[layer])
     this.log(`${description}: ${chalk.cyan(link)}`)
+  }
+
+  private async promptManualFunding(address: string, amount: number, layer: Layer) {
+    const chainId =
+      layer === Layer.L1 ? (await this.l1Provider.getNetwork()).chainId : (await this.l2Provider.getNetwork()).chainId
+
+    const qrString = `ethereum:${address}@${chainId}&value=${amount}`
+
+    await this.logAddress(address, `Please fund the following address with ${chalk.yellow(amount)} ETH`, layer)
+    this.log('\n')
+    this.log(`ChainID: ${chalk.cyan(Number(chainId))}`)
+    this.log(`Chain RPC: ${chalk.cyan(layer === Layer.L1 ? this.l1Rpc : this.l2Rpc)}`)
+    this.log('\n')
+    this.log('Scan this QR code to fund the address:')
+
+    this.log(await qrCodeToString(qrString, {small: true, type: 'terminal'}))
+
+    let funded = false
+    while (!funded) {
+      // eslint-disable-next-line no-await-in-loop
+      await confirm({message: 'Press Enter when ready...'})
+      this.log(`Checking...`)
+      // eslint-disable-next-line no-await-in-loop
+      const balance = await (layer === Layer.L1 ? this.l1Provider : this.l2Provider).getBalance(address)
+      const formattedBalance = ethers.formatEther(balance)
+
+      if (Number(formattedBalance) >= amount) {
+        this.log(chalk.green(`Wallet Balance: ${formattedBalance}`))
+        funded = true
+      } else {
+        this.log(chalk.yellow(`Balance is only ${formattedBalance}. Please fund the wallet.`))
+      }
+    }
+  }
+
+  private async promptUserForL2Funding(): Promise<string> {
+    const answer = await select({
+      choices: [
+        {name: 'Bridge funds from L1', value: 'bridge'},
+        {name: 'Directly fund L2 wallet', value: 'direct'},
+        {name: 'Manual funding', value: 'manual'},
+      ],
+      message: 'How would you like to fund the L2 address?',
+    })
+    return answer
   }
 }
--- test/commands/helper/fund-accounts.test.ts
+++ test/commands/helper/fund-accounts.test.ts
@@ -1,13 +1,13 @@
 import {runCommand} from '@oclif/test'
 import {expect} from 'chai'
 
-describe('helper:fund_devnet', () => {
+describe('helper:fund-accounts', () => {
   it('runs helper:fund_devnet cmd', async () => {
     const {stdout} = await runCommand('helper:fund_devnet')
     expect(stdout).to.contain('hello world')
   })
 
-  it('runs helper:fund_devnet --name oclif', async () => {
+  it('runs helper:fund-accounts --name oclif', async () => {
     const {stdout} = await runCommand('helper:fund_devnet --name oclif')
     expect(stdout).to.contain('hello oclif')
   })
